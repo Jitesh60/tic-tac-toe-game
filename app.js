b6da419c14493a8bbb7cf90029b8c3f1199fae0a
CodeYogi <>
2022-11-29 20:21:48 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,33 +1,66 @@
-let highest;
-let secondHighest;
+let counter = 0;
+let symbol;
 
-for(let i = 1; i > 0; i++) {
-  const numberString = prompt('next number?');
+function TicTacToe(event) {
+  const button = event.target;
+  counter++;
 
-  if(numberString === null) {
-    break;
-  }
-
-  const number = +numberString;
-
-  if(numberString === '' || isNaN(number)) {
-    continue;
-  }
-
-  console.log(`user entered: ${numberString}`, `Number conversion: ${number}`);
-
-  
-  if(highest === undefined || number > highest) {
-    highest = number;
+  if (counter % 2) {
+    symbol = "X";
+    button.innerHTML = `<div class="text-3xl text-white font-bold">${symbol}</div>`
   } else {
-    if(number !== highest && (secondHighest === undefined || number > secondHighest)) {
-      secondHighest = number;
+    symbol = "O";
+    button.innerHTML = `<div class="text-3xl text-white font-bold">${symbol}</div>`
+  }
+
+
+  button.disabled = true;
+
+  if(GameStart()===1){
+  }
+
+}
+
+function GameStart() {
+  
+
+  const buttonOne = document.getElementById('button-one').innerHTML;
+  const buttonTwo = document.getElementById('button-two').innerHTML;
+  const buttonThree = document.getElementById('button-three').innerHTML;
+  const buttonFour = document.getElementById('button-four').innerHTML;
+  const buttonFive = document.getElementById('button-five').innerHTML;
+  const buttonSix = document.getElementById('button-six').innerHTML;
+  const buttonSeven = document.getElementById('button-seven').innerHTML;
+  const buttonEight = document.getElementById('button-eight').innerHTML;
+  const buttonNine = document.getElementById('button-nine').innerHTML;
+
+  const Declared = document.getElementById('declared');
+
+  if(((buttonOne === buttonTwo && buttonTwo === buttonThree && buttonOne !== '') || (buttonOne === buttonFour && buttonOne === buttonSeven && buttonOne !== '') || (buttonOne === buttonFive && buttonOne === buttonNine && buttonOne !== '') || (buttonThree === buttonSix && buttonThree === buttonNine && buttonThree !== '') || (buttonThree === buttonFive && buttonThree === buttonSeven && buttonThree !== '') || (buttonFour === buttonFive && buttonFour === buttonSix && buttonFour !== '') || (buttonSeven === buttonEight && buttonSeven === buttonNine && buttonSeven !== '') || (buttonTwo === buttonFive && buttonTwo === buttonEight && buttonTwo != '')) && counter !==1) {
+    if (counter % 2) {
+      symbol = "X"
+            console.log('its working');
+      Declared.innerHTML = `<div class="text-white text-3xl font-bold"> player ${symbol} win</div>`
+    return 1;
+    }  else {
+      symbol = "O"
+      Declared.innerHTML = `<div class="text-white text-3xl font-bold">player ${symbol} win</div>`
+      return 1;
     }
   }
+  if(buttonOne !=='' && buttonTwo !=='' && buttonThree !=='' && buttonFour !=='' && buttonFive !=='' && buttonSix !=='' && buttonSeven !=='' && buttonEight !=='' && buttonNine !==''){
+    Declared.innerHTML = `<div class="text-white text-3xl font-bold">Draw</div>`
+  }
+  buttonOne.disabled = true;
+    buttonTwo.disabled = true;
+    buttonThree.disabled = true;
+    buttonFour.disabled = true;
+    buttonFive.disabled = true;
+    buttonSix.disabled = true;
+    buttonSeven.disabled = true;
+    buttonEight.disabled = true;
+    buttonNine.disabled = true;
 }
 
-if(secondHighest === undefined) {
-  document.write('No second highest found.');
-} else {
-  document.write(`Second Highest number is ${secondHighest}`);
-}
+
+
